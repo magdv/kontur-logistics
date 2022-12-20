ef8afae63601821aadd818f481ba1c00f2f54f57
Denis Korolev <d.korolev@magdv.com>
2022-12-20 14:59:27 +0300

    Fix фабрики

--- src/LogisticsFactory.php
+++ src/LogisticsFactory.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace MagDv\Logistics;
 
+use JMS\Serializer\SerializerInterface;
 use MagDv\Logistics\Interfaces\LogisticsDocumentsApiInterface;
 use MagDv\Logistics\Interfaces\LogisticsTransportationsApiInterface;
 use MagDv\Logistics\Interfaces\MintransGatewayApiInterface;
@@ -26,11 +27,12 @@ class LogisticsFactory
      */
     protected $serializer;
 
-    public function __construct(ClientInterface $client, string $apiKey, string $url = 'https://logist-api.kontur.ru/')
+    public function __construct(ClientInterface $client, string $apiKey, SerializerInterface $serializer, string $url = 'https://logist-api.kontur.ru/')
     {
         $this->apikey = $apiKey;
         $this->url = $url;
         $this->client = $client;
+        $this->serializer = $serializer;
     }
 
     public function getDocuments(): LogisticsDocumentsApiInterface
